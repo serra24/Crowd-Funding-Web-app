9a6e29844536c99315b965223a6885c03116f25b
fatenmaarek <faten.maarek2@gmail.com>
2024-03-07 13:13:20 +0200

    Your commit message

--- D_project/models.py
+++ D_project/models.py
@@ -30,4 +30,22 @@ class Image(models.Model):
     project=models.ForeignKey(Project,on_delete=models.CASCADE)
     image = models.FileField(upload_to='projects/images',blank=True,null=True)
     
+class Donation(models.Model):
+    donation = models.FloatField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(Register, on_delete=models.CASCADE)    
+
+
+    def __self__(self):
+        return self.donation
+
+
+
+class Rate(models.Model):
+    rating = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings')
+    # user = models.ForeignKey(Register, on_delete=models.CASCADE)    
     
+    def __str__(self):
+        return str(self.rating)    
--- D_project/urls.py
+++ D_project/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('<int:pk>/',project_details.as_view(),name="project_details"),
     path('',projects.as_view(),name='projects'),
     path('createProject',create_project,name="createProject"),
-    path('deleteProject/<int:pk>/',delete_project.as_view(),name="deleteProject")
+    path('deleteProject/<int:pk>/',delete_project.as_view(),name="deleteProject"),
+    path('donate/<int:project_id>/', donate, name='donate'),
 
 ]
