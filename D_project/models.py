1cb37ab58a74d34e0712e96786ccd9ab0ff1ff95
fatenmaarek <faten.maarek2@gmail.com>
2024-03-07 11:06:25 +0200

    donation and rating

--- D_project/models.py
+++ D_project/models.py
@@ -14,6 +14,7 @@ class Project(models.Model):
     description=models.TextField(default="loream loream",max_length=800,null=True)
     start=models.DateTimeField(auto_now_add=True)
     end=models.DateTimeField()
+    remaining_amount=models.FloatField(default=100,null=True)
     def __str__(self):
         return self.name
 
@@ -27,3 +28,26 @@ class Tags(models.Model):
     tag=models.CharField(max_length=50)
     def __str__(self):
         return self.tag
+    
+
+class Donation(models.Model):
+    donation = models.FloatField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(Register, on_delete=models.CASCADE)    
+
+
+    def __self__(self):
+        return self.donation
+
+
+
+class Rate(models.Model):
+    rating = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings')
+    # user = models.ForeignKey(Register, on_delete=models.CASCADE)    
+    
+    def __str__(self):
+        return str(self.rating)
+   
+    # rating = models.IntegerField(validators=[MaxValueValidator(5)])    
--- D_project/urls.py
+++ D_project/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import *
+from .views import projects, donate, project_details 
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('<int:pk>/',project_details.as_view(),name="project_details"),
-    path('',projects.as_view(),name='projects')
+    path('<int:pk>/', project_details.as_view(), name="project_details"),
+    path('', projects.as_view(), name='projects'),
+    path('donate/<int:project_id>/', donate, name='donate'),
+
 ]
