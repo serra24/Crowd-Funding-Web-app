e73091d9d160ec9c100fc23e321382294eae03a8
AyaElgedawy <ayaelgedawy4@gmail.com>
2024-03-09 21:27:24 +0200

    add Egyptian validation to phone number field

--- account/models.py
+++ account/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import RegexValidator
+
 # Create your models here.
 ''' 
     username
@@ -13,7 +15,14 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=11,null=True , blank=True)
+    phone_number = models.CharField(max_length=11,null=True , blank=True,
+                                    validators=[
+                                    RegexValidator(
+                                    regex=r'^01\d{9}$',
+                                    message='Phone number must start with 01 and be 9 digits long.',
+                                    code='invalid_phone_number',
+            ),
+        ],)
     addres = models.CharField(max_length=50 , blank=True, null=True)
     image= models.ImageField(upload_to='users/images',blank=True,null=True)
     # image
@@ -22,6 +31,23 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User , on_delete=models.CASCADE)
+#     phone_number = models.CharField(
+#         max_length=11,
+#         null=True,
+#         blank=True,
+#         validators=[
+#             RegexValidator(
+#                 regex=r'^01\d{9}$',
+#                 message='Phone number must start with 01 and be 10 digits long.',
+#                 code='invalid_phone_number',
+#             ),
+#         ],
+#     )
+#     addres = models.CharField(max_length=50 , blank=True, null=True)
+#     image= models.ImageField(upload_to='users/images',blank=True,null=True)
 
 
 @receiver(post_save , sender=User)
