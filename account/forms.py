0e5d9dd73e6eb3135f9b2ae78fccba8af4cb5af3
AyaElgedawy <ayaelgedawy4@gmail.com>
2024-04-18 05:02:56 +0200

    fix

--- account/forms.py
+++ account/forms.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username','email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+    password1 = forms.CharField(
+                widget=forms.PasswordInput(attrs={'class': 'form-control',"name": "Pasword",}),label = "Pasword"
+            )
+    password2 = forms.CharField(
+                widget=forms.PasswordInput(attrs={'class': 'form-control',"name": "Repeat your password",}),label = "Repeat your password"
+            )
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
